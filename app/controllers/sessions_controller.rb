0c3494c48b0eaf4e597a17c1247a6429c9a53019
h7110y <h7110y@gmail.com>
2014-03-13 10:01:18 +0900

    finish ch8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # todo signin & redirect to users#show
+      sign_in user
+      redirect_to user
+    else
+      # show error message
+      flash.now[:error] = 'invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
+    sign_out
+    redirect_to root_dir
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,27 @@
 class User < ActiveRecord::Base
   # すべてのメアドを小文字で保存する処理
-  # before_save { self.email = email.downcase }
+  # before_save { self.email = email.downcase } => 下の行と同じ挙動をする。なぜ？
   before_save { email.downcase! }
+  before_create :create_remember_token
   # validates (カラム名, {設定のハッシュ値})
   #   => メソッドの()と最後の引数の{}は省略可能
   validates :name, presence: true, length: { maximum: 50 }
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
